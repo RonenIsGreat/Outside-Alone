942497a74701ff93d0473bac7a5ef723990020da
Baruch <byahalom@gmail.com>
2018-12-09 16:03:53 +0200

    Added Opening Scene with Menu and Play and Exit buttons

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;// we need this namespace in order to access UI elements within our script
+
+public class MenuScript : MonoBehaviour {
+    public Canvas quitMenu;
+    public Button startText;
+    public Button exitText;
+    // Use this for initialization
+    void Start () {
+        quitMenu = quitMenu.GetComponent<Canvas>();
+        startText = startText.GetComponent<Button>();
+        exitText = exitText.GetComponent<Button>();
+        quitMenu.enabled = false;
+    }
+    public void ExitPress()
+    {
+        quitMenu.enabled = true;
+        startText.enabled = false;
+        exitText.enabled = false;
+    }
+
+    public void NoPress()
+    {
+        quitMenu.enabled = false;
+        startText.enabled = true;
+        exitText.enabled = true;
+    }
+
+    public void StartLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
