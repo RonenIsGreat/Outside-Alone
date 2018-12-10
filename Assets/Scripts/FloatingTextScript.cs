a9c7b55127b20e0025c99d66edcd920f644fe0b1
RonenIsGreat <40147121+RonenIsGreat@users.noreply.github.com>
2018-12-11 00:10:01 +0200

    Collectable rocks added

---
+++ Assets/Scripts/FloatingTextScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingTextScript : MonoBehaviour {
+
+    public MeshRenderer InfoText;
+    public float FadeSpeed = 3;
+    bool exitedStartArea = false;
+    Color infoTextOriginalColor;
+
+    private void Start()
+    {
+        infoTextOriginalColor = InfoText.material.color;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        ColorChange();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+            exitedStartArea = false;
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+            exitedStartArea = true;
+    }
+
+    private void ColorChange()
+    {
+        if (exitedStartArea)
+            InfoText.material.color = Color.Lerp(InfoText.material.color, Color.clear, FadeSpeed * Time.deltaTime);
+        else
+            InfoText.material.color = Color.Lerp(InfoText.material.color, infoTextOriginalColor, FadeSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/RayCastScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RayCastScript : MonoBehaviour {
+
+    public Text CollectRockText;
+    public Image Crosshair;
+    public Text RocksNumberText;
+    public int DistanceToSee = 3;
+    public int maxRocksToCollect = 5;
+
+    private Color originalCrosshairColor;
+    private int layerNumber;
+    private int collectedRocksNumber;
+
+	// Use this for initialization
+	void Start () {
+        originalCrosshairColor = Crosshair.color;
+        layerNumber = 8;
+        collectedRocksNumber = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+        Color colorForCrossHair = originalCrosshairColor;
+        int RayCastlayerMask = 1 << layerNumber;
+
+        if (Physics.Raycast(transform.position, transform.forward , out hit, DistanceToSee, RayCastlayerMask))
+        {
+            if (hit.collider.CompareTag("CollectableRock"))
+            {
+                colorForCrossHair = Color.green;
+                GameObject rock = hit.collider.gameObject;
+
+                // Check if player can collect more rocks
+                if (collectedRocksNumber < maxRocksToCollect)
+                {
+                    if (Input.GetKeyDown("e"))
+                    {
+                        Destroy(rock);
+                        collectedRocksNumber++;
+                        RocksNumberText.text = collectedRocksNumber.ToString();
+                    }
+                }
+            }
+        }
+
+        Crosshair.color = colorForCrossHair;
+    }
+}
