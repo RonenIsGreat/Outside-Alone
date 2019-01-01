9fe81b41b4b37371d40711c4983c1441fb960f9d
Baruch <byahalom@gmail.com>
2019-01-01 19:03:15 +0200

    Added 2D Intro scene

---
+++ Assets/Scripts/ZombieFriendController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieFriendController : MonoBehaviour {
+    public Transform target;
+    public float engaugeDistance = 10f;
+    public float moveSpeed = 5f;
+    private bool isFacingLeft = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance(target.position,this.transform.position) < engaugeDistance)
+        {
+            Vector3 targetDirection = target.position - this.transform.position;
+
+            if (Mathf.Sign(targetDirection.x) == 1 && isFacingLeft)
+            {
+                Flip();
+            } else if (Mathf.Sign(targetDirection.x) == -1 && !isFacingLeft)
+            {
+                Flip();
+            }
+        }
+        else if (Vector3.Distance(target.position, this.transform.position) < engaugeDistance)
+        {
+            Debug.DrawLine(target.position, this.transform.position, Color.green);
+        }
+	}
+
+    private void Flip()
+    {
+        isFacingLeft = !isFacingLeft;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
