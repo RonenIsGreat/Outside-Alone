9af1bc68dcbefac5d9ebe924ca337add191016bf
RonenIsGreat <40147121+RonenIsGreat@users.noreply.github.com>
2019-01-03 20:46:41 +0200

    Added more destruction to the tutorial and change scene when all objects destroyed

---
+++ Assets/Scripts/TutorialFinishedScrpit.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialFinishedScrpit : MonoBehaviour {
+
+    public List<GameObject> objectsToDestroy = new List<GameObject>();
+	
+	// Update is called once per frame
+	void Update () {
+        if (areAllObjectsDestroyed())
+        {
+            StartCoroutine(ChangeScene());
+        }
+	}
+
+    private IEnumerator ChangeScene()
+    {
+        //Wait for 5 seconds
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene(3);
+    }
+
+    private bool areAllObjectsDestroyed()
+    {
+        foreach (var item in objectsToDestroy)
+        {
+            if (item != null)
+                return false;
+        }
+
+        return true;
+    }
+}
