df5d658b14c8446c01a94961524caa16daf753f9
RonenIsGreat <40147121+RonenIsGreat@users.noreply.github.com>
2019-01-04 16:46:30 +0200

    Running zombie if player gets too close, going back patroling if player got too far

--- Assets/Scripts/ZombieScript.cs
+++ Assets/Scripts/ZombieScript.cs
@@ -5,6 +5,9 @@ using UnityEngine.AI;
 
 public class ZombieScript : MonoBehaviour {
     public List<Transform> waypoints = new List<Transform>();
+    public Transform player;
+    public float distanceToSee = 10;
+
     // The waypoint currently targeted
     private Transform targetWaypoint;
     private int targetWaypointIndex = 0;
@@ -12,6 +15,9 @@ public class ZombieScript : MonoBehaviour {
     private float minDistance = 0.1f;
     private int lastWaypointIndex;
     private NavMeshAgent agent;
+    private Animator zombieAnimator;
+    private float walkSpeed;
+    private float runSpeed;
 
     // Use this for initialization
     void Start()
@@ -21,13 +27,27 @@ public class ZombieScript : MonoBehaviour {
         targetWaypoint = waypoints[targetWaypointIndex];
         agent = GetComponent<NavMeshAgent>();
         agent.destination = targetWaypoint.position;
+        zombieAnimator = GetComponent<Animator>();
+        walkSpeed = agent.speed;
+        runSpeed = walkSpeed * 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!agent.pathPending && (agent.remainingDistance < minDistance))
+        if (Vector3.Distance(transform.position, player.position) <= distanceToSee)
+        {
+            runToTarget(player);
+        }
+        else if (!agent.pathPending && (agent.remainingDistance < minDistance))
             GotoNextPoint();
+    }
+
+    void runToTarget(Transform target)
+    {
+        agent.speed = runSpeed;
+        zombieAnimator.speed = runSpeed;
+        agent.destination = target.position;
     }
 
     void UpdateTargetWayPoint()
@@ -48,6 +68,8 @@ public class ZombieScript : MonoBehaviour {
             return;
 
         UpdateTargetWayPoint();
+        agent.speed = walkSpeed;
+        zombieAnimator.speed = walkSpeed;
         agent.destination = targetWaypoint.position;
     }
 }
