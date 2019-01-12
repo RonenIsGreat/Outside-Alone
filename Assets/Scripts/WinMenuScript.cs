c2fca3c6ab7740366966276348c2c2f609972621
Baruch <byahalom@gmail.com>
2019-01-12 14:28:16 +0200

    Add winner scene with 'back to main menu'button

---
+++ Assets/Scripts/WinMenuScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;// we need this namespace in order to access UI elements within our script
+
+public class WinMenuScript : MonoBehaviour {
+    public Button toMainText;
+    // Use this for initialization
+    void Start()
+    {
+        toMainText = toMainText.GetComponent<Button>();
+    }
+
+    public void ToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
